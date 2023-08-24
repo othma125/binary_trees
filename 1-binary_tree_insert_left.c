a2a9fa7add908874dac0664e3861d57167ae9fcf
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-24 20:19:31 +0100

    3-binary_tree_delete.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *p, int n)
 {
 	binary_tree_t *new = binary_tree_node(p, n);
-	
+
 	if (new == NULL)
 		return (NULL);
 	if (p->left != NULL)
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *p, int n)
 {
 	binary_tree_t *new = binary_tree_node(p, n);
-	
+
 	if (new == NULL)
 		return (NULL);
 	if (p->right != NULL)
