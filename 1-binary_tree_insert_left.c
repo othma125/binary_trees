0a16b6d1672a93c464136386cb65279feba28a50
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-24 19:58:26 +0100

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - check code
+ * binary_tree_insert_left - check code
  * @p: binary_tree_t
  * @n: int
  * Return: new tree node
  */
-binary_tree_t *binary_tree_node(binary_tree_t *p, int n)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *p, int n)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 	
@@ -16,5 +16,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *p, int n)
 	new->parent = p;
 	new->left = NULL;
 	new->right = NULL;
+	p->left = new;
 	return (new);
 }
