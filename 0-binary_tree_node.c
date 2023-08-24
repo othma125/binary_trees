99edd011e13cc77c29720c0326e318d13388401d
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-24 17:36:36 +0100

    binary _tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,9 +9,8 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *p, int n)
 {
-	binary_tree_t *new;
-
-	new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
