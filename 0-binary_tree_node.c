9bf705e95ff5932c2d16d8a86262c1004910c5a8
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-24 16:59:33 +0100

    first commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_node - check code
+ * @p: binary_tree_t
+ * @n: int
+ * Return: new tree node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *p, int n)
+{
+  binary_tree_t *new;
+
+  new = malloc(sizeof(binary_tree_t));
+  if (new == NULL)
+    return (NULL);
+  new->n = n;
+  new->parent = p;
+  new->left = NULL;
+  new->right = NULL;
+  return (new);
+}
