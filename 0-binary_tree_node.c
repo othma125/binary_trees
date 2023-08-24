ceae347a973c3bab99d5873b398c812a80efee67
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-24 09:10:34 -0700

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,14 +9,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *p, int n)
 {
-  binary_tree_t *new;
+	binary_tree_t *new;
 
-  new = malloc(sizeof(binary_tree_t));
-  if (new == NULL)
-    return (NULL);
-  new->n = n;
-  new->parent = p;
-  new->left = NULL;
-  new->right = NULL;
-  return (new);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+	  return (NULL);
+	new->n = n;
+	new->parent = p;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
 }
