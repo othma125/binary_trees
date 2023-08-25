9079e0c25a695f836fec2b837fa03a211b7c0d0c
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-25 15:37:31 +0100

    15-binary_tree_is_full.c

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - check code
+ * @tree: binary_tree_t node
+ * Return: an integer
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left = 1 + binary_tree_is_full(tree->left);
+	if (tree->right)
+		right = 1 + binary_tree_is_full(tree->right);
+	if (left == right)
+		return (1);
+	return (0);
+}
