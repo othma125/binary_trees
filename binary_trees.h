c714185fb9a9fd5597add210f5effb9f1bd428d6
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-25 13:00:36 +0100

    8-binary_tree_postorder.c

--- binary_trees.h
+++ binary_trees.h
@@ -29,4 +29,5 @@ int binary_tree_is_leaf(const binary_tree_t *);
 int binary_tree_is_root(const binary_tree_t *);
 void binary_tree_preorder(const binary_tree_t *, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *, void (*func)(int));
 #endif
