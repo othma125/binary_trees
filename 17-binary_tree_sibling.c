56ad11d3ae90370bee61eab15b74202e8e096479
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-25 16:57:16 +0100

    17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+	
+/**
+ * binary_tree_sibling - check code
+ * @node: binary_tree_t node
+ * Return: binary_tree_t node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else if (node->parent->right == node)
+		return(node->parent->left);
+}
