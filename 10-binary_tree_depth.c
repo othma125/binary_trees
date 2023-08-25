8534514c6da4be5f8ba58d04fec213d1ce1535c0
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-25 15:16:51 +0100

    11-binary_tree_size.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,7 +9,7 @@ size_t binary_tree_depth(const binary_tree_t *node)
 {
 	size_t depth = 0;
 
-	if (node == NULL)
+	if (!node)
 		return (0);
 	if (node->parent)
 		depth = 1 + binary_tree_depth(node->parent);
---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - check code
+ * @tree: binary_tree_t node
+ * Return: an integer
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		size += binary_tree_size(tree->left);
+	if (tree->right)
+		size += binary_tree_size(tree->right);
+	return (size + 1);
+}
