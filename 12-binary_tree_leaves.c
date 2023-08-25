3c899596fd05f6dc50ac474a68b5a4cf090b1638
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-25 15:27:45 +0100

    12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - check code
+ * @tree: binary_tree_t node
+ * Return: an integer
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	int leaves = 0;
+
+	if (!tree)
+		return (leaves);
+	if (!tree->left && !tree->right)	
+		return (1);
+	if (tree->left)
+		leaves += binary_tree_leaves(tree->left);
+	if (tree->right)
+		leaves += binary_tree_leaves(tree->right);
+	return (leaves);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -33,4 +33,5 @@ void binary_tree_postorder(const binary_tree_t *, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *);
 size_t binary_tree_depth(const binary_tree_t *);
 size_t binary_tree_size(const binary_tree_t *);
+size_t binary_tree_leaves(const binary_tree_t *);
 #endif
