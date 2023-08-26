c68b5d42af69ac9cb95d5a3f5f9cfacb9d4dcf86
bensbahou <younesbensbahou@gmail.com>
2023-08-26 20:45:29 +0100

    proto heap insert

--- binary_trees.h
+++ binary_trees.h
@@ -53,4 +53,5 @@ bst_t *bst_remove(bst_t *, int);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *array_to_avl(int *array, size_t size);
+heap_t *heap_insert(heap_t **root, int value);
 #endif
