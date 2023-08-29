c4f49d83bc8caf0fe9dbc420eadb93cb103872ad
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-29 11:50:57 +0100

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,12 +10,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *p, int n)
 {
 	binary_tree_t *new = binary_tree_node(p, n);
 
-	if (new == NULL)
+	if (!new || !p)
 		return (NULL);
-	if (p->left != NULL)
+	if (p->left)
 	{
 		new->left = p->left;
-		p->left->parent = new;
+		new->left->parent = new;
 	}
 	p->left = new;
 	return (new);
