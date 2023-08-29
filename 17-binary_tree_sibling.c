040f3c43da631956bd27d63b0e203e9012b84b65
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-29 12:38:32 +0100

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,6 +11,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	if (node->parent->left == node)
 		return (node->parent->right);
-	else if (node->parent->right == node)
-		return (node->parent->left);
+	return (node->parent->left);
 }
