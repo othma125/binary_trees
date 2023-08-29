cccf8d7afca2078ada8b6302e50fcfa3df9867a6
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-29 12:19:02 +0100

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,24 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height - check code
+ * @node: binary_tree_t node
+ * Return: an integer
+ */
+size_t binary_tree_height(const binary_tree_t *node)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
 
+	if (node == NULL)
+		return (0);
+	if (node->left)
+		left_height = 1 + binary_tree_height(node->left);
+	if (node->right)
+		right_height = 1 + binary_tree_height(node->right);
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
+}
 /**
  * binary_tree_balance - check code
  * @tree: binary_tree_t node
@@ -7,13 +26,13 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	size_t left = 0, right = 0;
 
 	if (!tree)
 		return (0);
 	if (tree->left)
-		left = 1 + binary_tree_balance(tree->left);
+		left = binary_tree_height(tree->left);
 	if (tree->right)
-		right = 1 + binary_tree_balance(tree->right);
+		right = binary_tree_height(tree->right);
 	return (left - right);
 }
