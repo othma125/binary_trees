f29e2df8d1cd91a433b9be509982a90c3e951d55
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-29 15:06:58 +0100

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,5 +1,17 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_depth - check code
+ * @tree: binary_tree_t node
+ * Return: integer
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (d1 + binary_tree_depth(tree->parent));
+}
 /**
  * binary_tree_ancestor - check code
  * @node1: binary_tree_t node
@@ -9,16 +21,34 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *node1
 , const binary_tree_t *node2)
 {
-	binary_tree_t *father = node1->parent;
-	binary_tree_t *mother = node2->parent;
+	size_t depth1, depth2;
 
 	if (!node1 || !node1)
 		return (NULL);
-	if (node1 == node2)
-		return ((binary_tree_t *)node1);
-	if (node1 == mother || !father || (!father->parent && mother))
-		return (binary_trees_ancestor(node1, mother));
-	if (node2 == father || !mother || (!mother->parent && father))
-		return (binary_trees_ancestor(father, node2));
-	return (binary_trees_ancestor(father, mother));
+	depth1 = binary_tree_depth(node1);
+	depth2 = binary_tree_depth(node2);
+	if (depth1 > depth2)
+	{
+		while (depth1 != depth2)
+		{
+			node1 = node1->parent;
+			depth1--;
+		}
+	}
+	else if (depth2 > depth1)
+	{
+		while (depth2 != depth1)
+		{
+			node2 = node2->parent;
+			depth2--;
+		}
+	}
+	while (node1 && node2)
+	{
+		if (node1 == node2)
+			return ((binary_tree_t *)node1);
+		node1 = node1->parent;
+		node2 = node2->parent;
+	}
+	return ((binary_tree_t *)node1);
 }
