1f6ad041c76f156ac6038bb953df43a12cdf52fb
bensbahou <younesbensbahou@gmail.com>
2023-08-30 22:32:42 +0100

    check 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,15 +2,18 @@
 
 /**
  * binary_tree_size - check code
+ *
  * @tree: binary_tree_t node
+ *
  * Return: an integer
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
+	size_t size;
 
 	if (!tree)
 		return (0);
+	size = 0;
 	if (tree->left)
 		size += binary_tree_size(tree->left);
 	if (tree->right)
