c3aa6847c36af5081b76560d9714080d662d7f2f
bensbahou <younesbensbahou@gmail.com>
2023-08-30 22:31:20 +0100

    check 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,10 +7,11 @@
  */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
-	size_t depth = 0;
+	size_t depth;
 
 	if (!node)
 		return (0);
+	depth = 0;
 	if (node->parent)
 		depth = 1 + binary_tree_depth(node->parent);
 	return (depth);
