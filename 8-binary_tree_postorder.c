1fc0331a31948facc26871cd17a6ed09373ca159
bensbahou <younesbensbahou@gmail.com>
2023-08-30 22:29:56 +0100

    check 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,8 +2,10 @@
 
 /**
  * binary_tree_postorder - check code
+ *
  * @tree: binary_tree_t
  * @func: function pointer
+ *
  * Return: nothing
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
